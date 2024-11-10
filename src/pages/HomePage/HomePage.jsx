130af8233c02de49111ac82d6e32a4025f1991a5
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-11-10 22:14:39 +0100

    123

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,9 @@
 import Header from '../../components/Header/Header.jsx';
 import HomeContent from '../../components/HomeContent/HomeContent.jsx';
-import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div className={css.homeContainer}>
+    <div>
       <Header />
       <HomeContent />
     </div>
